7f5d77cbd845301b93b9fd18b9082ff324169abb
MohammadMahabuburRahman <138484048+MohammadMahabuburRahman@users.noreply.github.com>
2023-09-14 08:35:22 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+
+import './App.css'
+import Header from './components/header/header'
+import Blogs from './components/Blogs/Blogs'
+
+function App() {
+
+  return (
+    <>
+      
+      <Header></Header>
+      <div>
+      <Blogs></Blogs>
+      </div>
+      
+    </>
+  )
+}
+
+export default App
